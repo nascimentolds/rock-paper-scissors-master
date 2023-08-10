5b771ba24708f9beba196e646529e6c1bb3e7604
nascimentolds <nascimentolds@hotmail.com>
2023-08-10 17:09:43 -0300

    win message

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const rulesBt = document.querySelector('.rules-bt');
 const closeBt = document.querySelector('.close-bt');
 const modal = document.querySelector('.rules');
+const winBox = document.querySelector('.win');
+const winMessage = document.querySelector('.win-message');
 
 rulesBt.addEventListener('click', () => {
   modal.style.display = 'flex';
@@ -28,13 +30,42 @@ options.forEach(option => {
     playerPicked(option.id);
     var housePick = Math.floor(Math.random() * 3);
     housePicked(housePick);
-    // setTimeout(housePicked(housePick), 3000);
 
+    var winner = win(option.id, housePick);
+    winMessageDisplay(winner);
+    console.log(winPlayer, winHouse);
+
+    setTimeout(() => {
+      winBox.style.display = 'flex';
+    }, 1000); 
   });
 })
 
-// options
+// win
+let winPlayer = 0
+let winHouse = 0
+function win(p1, p2) {
+  if (p2 == 0 && p1 == 1 || p2 == 1 && p1 == 2 || p2 == 2 && p1 == 0) {
+    winPlayer++
+    return "winner";
+  }
+  if (p1 == 0 && p2 == 1 || p1 == 1 && p2 == 2 || p1 == 2 && p2 == 0) {
+    winHouse--
+    return "loser";
+  }
+}
 
+function winMessageDisplay(win) {
+  if (win == "winner") {
+    winMessage.innerHTML = "YOU WIN";
+  } else if (win == "loser") {
+    winMessage.innerHTML = "YOU LOSE";
+  } else {
+    winMessage.innerHTML = "DRAW";
+  }
+}
+
+// options
 function playerPicked(player) {
   if (player == 0) {
     return playerChosse.innerHTML = "<div class='border border-paper'><div class='button'><img src='./images/icon-paper.svg' alt='paper' class='img-paper' /></div></div>"
@@ -51,17 +82,17 @@ function housePicked(house) {
   if (house == 0) {
     return setTimeout(function () {
       houseChosse.innerHTML = "<div class='border border-paper'><div class='button'><img src='./images/icon-paper.svg' alt='paper' class='img-paper' /></div></div>"
-    }, 1000);
+    }, 700);
   }
   if (house == 1) {
     return setTimeout(function () {
       houseChosse.innerHTML = "<div class='border border-scissors'><div class='button'><img src='./images/icon-scissors.svg' alt='scissors' class='img-scissors' /></div></div>"
-    }, 1000);
+    }, 700);
   }
   if (house == 2) {
     return setTimeout(function () {
       houseChosse.innerHTML = "<div class='border border-rock'><div class='button'><img src='./images/icon-rock.svg' alt='rock' class='img-rock' /></div></div>"
-    }, 1000);
+    }, 700);
   }
 }
 
@@ -72,4 +103,5 @@ winBtn.addEventListener('click', () => {
   game.style.display = 'none';
   opcoes.style.display = 'block';
   houseChosse.innerHTML = '<div class="hide-choose"></div>';
+  winBox.style.display = 'none';
 })
